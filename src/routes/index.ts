d448d7f66e8703c210976977691dabf5faa8716a
sakibsarkar <sakibsarkar707@gmail.com>
2024-07-02 17:42:20 +0600

    authentiction route connect

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import authRoute from "./auth.route";
 const router = express.Router();
 
+const moduleRoute = [
+  {
+    path: "/auth",
+    route: authRoute,
+  },
+];
 
+moduleRoute.forEach((route) => router.use(route.path, route.route));
 
 export default router;
